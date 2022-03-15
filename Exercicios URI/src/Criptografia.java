53133f90bdcb2599b235a459a578607a0dc168f2
Vinícius Barros <vini_dsbarros@hotmail.com>
2022-03-15 11:44:43 -0300

    Upload exercício
    Upload do exercício 1168 - LED

---
+++ Exercicios URI/src/Criptografia.java
@@ -0,0 +1,11 @@
+
+public class Criptografia {
+
+	public static void main(String[] args) {
+		char ascii[] = new char[256];
+		for(int i = 0; i < 256; i++)
+			System.out.println(ascii);
+
+	}
+
+}
