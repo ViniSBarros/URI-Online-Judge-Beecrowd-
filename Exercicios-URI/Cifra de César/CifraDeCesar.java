1c44490b04154ed4ebbe5d6718f04335028ce717
Vinícius Barros <72898323+ViniSBarros@users.noreply.github.com>
2022-04-20 18:16:41 -0300

    Update CifraDeCesar.java

--- Exercicios-URI/Cifra de César/CifraDeCesar.java
+++ Exercicios-URI/Cifra de César/CifraDeCesar.java
@@ -11,7 +11,8 @@
  * 
  * Entrada
  * 
- * A entrada contém vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. Cada caso de teste é composto por duas linhas. A primeira linha contém uma string 
+ * A entrada contém vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. Cada caso de teste é composto por
+ * duas linhas. A primeira linha contém uma string 
  * com até 50 caracteres maiúsculos ('A'-'Z'), que é a sentença após ela ter sido codificada através desta Cifra de César modificada. A segunda linha contém um número 
  * que varia de 0 a 25 e que representa quantas 
  * posições cada letra foi deslocada para a direita.
